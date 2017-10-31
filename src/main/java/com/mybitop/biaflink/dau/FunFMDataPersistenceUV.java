7462254550438c3ca8f248c8221fca2582dbefd7
mustangkong <mustang2247@gmail.com>
2017-10-31 23:03:14 +0800

    add kafka to db

--- src/main/java/com/mybitop/biaflink/dau/FunFMDataPersistenceUV.java
+++ src/main/java/com/mybitop/biaflink/dau/FunFMDataPersistenceUV.java
@@ -3,44 +3,40 @@ package com.mybitop.biaflink.dau;
 import com.mybitop.biaflink.redis.RedisManager;
 import org.apache.flink.api.common.io.OutputFormat;
 import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.api.java.tuple.Tuple3;
 import org.apache.flink.configuration.Configuration;
+import org.apache.flink.streaming.api.functions.sink.RichSinkFunction;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import redis.clients.jedis.Jedis;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
 
 /**
  * 将数据写入MySQL中 5
  * 或者缓存到Redis中
  */
-public class FunFMDataPersistenceUV implements OutputFormat<Tuple2<String, Integer>> {
+public class FunFMDataPersistenceUV extends RichSinkFunction<Tuple3<String, String, String>> {
     private static final Logger logger = LoggerFactory.getLogger( FunFMDataPersistenceUV.class );
 
-    private Jedis jedis = null;
+    private Connection connection;
+    private PreparedStatement preparedStatement;
+
     @Override
-    public void configure(Configuration configuration) {
-        RedisManager.getJedis();
-        logger.info("FunFMDataPersistenceUV   configure");
+    public void open(Configuration parameters) throws Exception {
+        super.open(parameters);
     }
 
     @Override
-    public void open(int i, int i1) throws IOException {
-        if(jedis == null){
-            jedis = RedisManager.getJedis();
-        }
+    public void invoke(Tuple3<String, String, String> stringStringStringTuple3) throws Exception {
 
-        logger.info("FunFMDataPersistenceUV   open");
     }
 
     @Override
-    public void writeRecord(Tuple2<String, Integer> record) throws IOException {
-        logger.info("FunFMDataPersistenceUV   writeRecord");
-        jedis.hset("startup", record.getField(0), record.getField(1));
+    public void close() throws Exception {
+        super.close();
     }
 
-    @Override
-    public void close() throws IOException {
-
-    }
 }
